ff95057114e61894a75e621d4d95bdf40ab084e0
vanholme <van.seryakov@gmail.com>
2021-06-10 14:14:12 +0300

    add dev-server

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,8 +1,19 @@
+const webpack = require('webpack')
 const { merge } = require('webpack-merge')
 
 const common = require('./webpack.common')
 const paths = require('./paths')
 
 module.exports = merge(common, {
-    mode: 'development'
+    mode: 'development',
+    devServer: {
+        contentBase: paths.dist,
+        open: true,
+        liveReload: true,
+        port: 8080
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
 })
